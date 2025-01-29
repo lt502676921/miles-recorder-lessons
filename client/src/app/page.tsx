d3991e62729dae226aeb0488f766c52e524dbbac
oreo <Tang.Li5@geely.com>
2025-01-29 09:49:51 +0800

    style: add some style

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -9,7 +9,12 @@ export default function Home() {
   return (
     <div className="w-[100vw] h-[100vh] p-4 bg-black">
       <div className="rounded-2xl h-full ring-1 ring-zinc-800 px-8 py-8 flex flex-col">
-        <div className='text-white'>{recording ? 'Recording' : 'Not Recording'}</div>
+        <div className="w-full flex items-center justify-between pb-4">
+          <div className="flex items-center text-2xl font-semibold leading-none tracking-tight text-white">
+            Miles's Recorder
+          </div>
+          <div className="text-white">{recording ? 'Recording' : 'Not Recording'}</div>
+        </div>
       </div>
     </div>
   );
